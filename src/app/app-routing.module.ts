7595c14d243a25fcedcb1f5a4aedcf481314a8f5
kehindeoluseyi18 <kehindeoluseyi18@gmail.com>
2023-05-24 12:22:14 +0100

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DistanceComponent } from './home/component/distance/distance.component';
+// import { DestinationComponent } from './home/component/destination/destination.component';
+import { CategoriesComponent } from './home/component/categories/categories.component';
+import { ExperienceComponent } from './home/component/experience/experience.component';
+import { StaysComponent } from './home/component/stays/stays.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent,
+  children: [
+    {
+      path: 'destination',
+      component: DistanceComponent
+    },
+    { path: 'categories',
+    component: CategoriesComponent
+  },
+  {
+    path: 'experiences',
+    component: ExperienceComponent
+  },
+  {
+    path: 'stays',
+    component: StaysComponent
+  }
+  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { DistanceComponent } from './home/component/distance/distance.component';
+import { ExperienceComponent } from './home/component/experience/experience.component';
+import { CategoriesComponent } from './home/component/categories/categories.component';
+import { StaysComponent } from './home/component/stays/stays.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    DistanceComponent,
+    ExperienceComponent,
+    CategoriesComponent,
+    StaysComponent
   ],
   imports: [
     BrowserModule,
